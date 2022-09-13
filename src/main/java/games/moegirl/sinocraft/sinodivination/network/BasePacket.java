54c1eeaa721a293f219095868546630eea9a0f4e
luqin2007 <lq2007lq@hotmail.com>
2022-09-13 11:18:19 +0800

    remove all clientonly

--- src/main/java/games/moegirl/sinocraft/sinodivination/network/BasePacket.java
+++ src/main/java/games/moegirl/sinocraft/sinodivination/network/BasePacket.java
@@ -1,8 +1,7 @@
 package games.moegirl.sinocraft.sinodivination.network;
 
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.util.function.Supplier;
@@ -22,15 +21,18 @@ public abstract class BasePacket<T extends BasePacket<T>> {
     public void consume(Supplier<NetworkEvent.Context> contextSupplier) {
         NetworkEvent.Context context = contextSupplier.get();
         if (context.getDirection().getReceptionSide().isServer()) {
-            context.enqueueWork(() -> onServer(context));
+            context.enqueueWork(() -> onServer(context, context.getSender()));
         } else {
             context.enqueueWork(() -> onClient(context));
         }
         context.setPacketHandled(true);
     }
 
-    @OnlyIn(Dist.CLIENT)
-    protected abstract void onClient(NetworkEvent.Context context);
+    protected void onClient(NetworkEvent.Context context) {
+        throw new IllegalStateException("Packet " + getClass().getSimpleName() + " can only execute at server side.");
+    }
 
-    protected abstract void onServer(NetworkEvent.Context context);
+    protected void onServer(NetworkEvent.Context context, Player sender) {
+        throw new IllegalStateException("Packet " + getClass().getSimpleName() + " can only execute at client side.");
+    }
 }
--- src/main/java/games/moegirl/sinocraft/sinodivination/network/PktS2CTripodInterrupt.java
+++
@@ -1,4 +0,0 @@
-package games.moegirl.sinocraft.sinodivination.network;
-
-public class PktS2CTripodInterrupt {
-}
