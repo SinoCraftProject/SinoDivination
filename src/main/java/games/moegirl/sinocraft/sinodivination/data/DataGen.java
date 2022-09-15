b6bd8f11ae192052d291db0bd14df3976c72a0a3
秋雨落 <i@rain.cx>
2022-09-15 08:13:00 +0800

    feat: run data gen, use dev branch of sc.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinodivination/data/DataGen.java
+++ src/main/java/games/moegirl/sinocraft/sinodivination/data/DataGen.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = SinoDivination.MODID)
-public class DataGenRegister {
+public class DataGen {
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent event) {
         if (event.includeClient()) {
